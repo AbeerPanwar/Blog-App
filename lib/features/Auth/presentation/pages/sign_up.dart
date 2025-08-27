460fddd64b86e433ba9b4c04ee5794cf6d2734a5
AbeerPanwar <panwarabeer6@gmail.com>
2025-08-27 20:52:50 +0530

    added loading and failure states when signing up

--- lib/features/Auth/presentation/pages/sign_up.dart
+++ lib/features/Auth/presentation/pages/sign_up.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/core/widgets/loading.dart';
+import 'package:blog_app/core/widgets/show_snackbar.dart';
 import 'package:blog_app/features/Auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/Auth/presentation/widgets/auth_button.dart';
 import 'package:blog_app/features/Auth/presentation/widgets/auth_field.dart';
@@ -41,79 +43,95 @@ class _SignUpPageState extends State<SignUpPage> {
         child: GlassBox(
           width: MediaQuery.of(context).size.width - 50,
           height: 550,
-          child: Form(
-            key: formkey,
-            child: Column(
-              children: [
-                Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Log in to manage your blogs',
-                  style: TextStyle(fontSize: 16, color: Colors.white54),
-                ),
-                SizedBox(height: 30),
-                AuthField(
-                  hintText: 'Name',
-                  suffixIcon: Icon(Icons.person),
-                  controller: nameController,
-                ),
-                SizedBox(height: 15),
-                AuthField(
-                  hintText: 'Email',
-                  suffixIcon: Icon(Icons.email_outlined),
-                  controller: emailController,
-                ),
-                SizedBox(height: 15),
-                AuthField(
-                  hintText: 'Password',
-                  suffixIcon: Icon(Icons.visibility_off),
-                  controller: passwordController,
-                  isPassword: true,
-                ),
-                SizedBox(height: 30),
-                AuthButton(
-                  text: 'Sign Up',
-                  onTap: () {
-                    if (formkey.currentState!.validate()) {
-                      context.read<AuthBloc>().add(
-                        AuthSignUp(
-                          name: nameController.text.trim(),
-                          email: emailController.text.trim(),
-                          password: passwordController.text.trim(),
-                        ),
-                      );
-                    }
-                  },
-                ),
-                SizedBox(height: 10), 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          child: BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if(state is AuthFailure){
+                showSnackbar(context, state.message);
+              }
+            },
+            builder: (context, state) {
+              if(state is AuthLoading){
+                return Loading();
+              }
+
+              return Form(
+                key: formkey,
+                child: Column(
                   children: [
                     Text(
-                      'Already have an account?  ',
-                      style: TextStyle(color: Colors.white54, fontSize: 16),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                        Navigator.pushNamed(context, 'signInPage');
-                      },
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.green.shade800,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                        ),
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Log in to manage your blogs',
+                      style: TextStyle(fontSize: 16, color: Colors.white54),
+                    ),
+                    SizedBox(height: 30),
+                    AuthField(
+                      hintText: 'Name',
+                      suffixIcon: Icon(Icons.person),
+                      controller: nameController,
+                    ),
+                    SizedBox(height: 15),
+                    AuthField(
+                      hintText: 'Email',
+                      suffixIcon: Icon(Icons.email_outlined),
+                      controller: emailController,
+                    ),
+                    SizedBox(height: 15),
+                    AuthField(
+                      hintText: 'Password',
+                      suffixIcon: Icon(Icons.visibility_off),
+                      controller: passwordController,
+                      isPassword: true,
+                    ),
+                    SizedBox(height: 30),
+                    AuthButton(
+                      text: 'Sign Up',
+                      onTap: () {
+                        if (formkey.currentState!.validate()) {
+                          context.read<AuthBloc>().add(
+                            AuthSignUp(
+                              name: nameController.text.trim(),
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim(),
+                            ),
+                          );
+                        }
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?  ',
+                          style: TextStyle(color: Colors.white54, fontSize: 16),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                            Navigator.pushNamed(context, 'signInPage');
+                          },
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.green.shade800,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
